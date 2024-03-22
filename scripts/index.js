c7bd2a22831de8c712af30877789769012c3c25b
matinwk <promatinwk@gmail.com>
2024-03-22 21:21:58 +0100

    finish navbar with js manipulation

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
+const navbar = document.querySelector('nav');
+const registerForm = document.querySelector('.register');
+const registerBtn = document.querySelector('.registerButton');
+
+
+let formVisible = false;
+
+
+const showMenuOnMobile = hamburgerMenu.addEventListener('click',()=>{
+    navbar.classList.toggle('active');
+})
+const showRegisterForm = registerBtn.addEventListener('click',()=>{
+    formVisible = !formVisible;
+    if (formVisible){
+        registerForm.style.display = 'block';
+    } else{
+        registerForm.style.display = 'none';
+    }
+})
