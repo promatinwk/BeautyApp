f34073c8759a438102a37578e2cf1418d990dc43
matinwk <promatinwk@gmail.com>
2024-04-08 18:25:25 +0200

    selected worker and show description (D3/30)

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector('.hamburgerMenu');
 const navbar = document.querySelector('nav');
 const registerForm = document.querySelector('.register');
 const registerBtn = document.querySelector('.registerButton');
+const workers = document.querySelectorAll('.worker');
 
 
 let formVisible = false;
@@ -18,3 +19,28 @@ const showRegisterForm = registerBtn.addEventListener('click',()=>{
         registerForm.style.display = 'none';
     }
 })
+
+
+let currentWorker = null;
+workers.forEach((worker)=>{
+    let workerPhoto = worker.querySelector('.worker-photo');
+    let workerDescription = worker.querySelector('.worker-description');
+        workerPhoto.addEventListener('click', () => {
+        // Jeśli wcześniej był wybrany inny pracownik, ukryj jego opis
+        if (currentWorker !== null) {
+            currentWorker.description.style.display = 'none';
+           // currentWorker.description.classList.remove('selected-worker-description')
+            currentWorker.photo.classList.remove('selected-worker');
+        }
+        currentWorker = {
+            photo: workerPhoto,
+            description: workerDescription
+        };
+        currentWorker.description.style.display = 'flex';
+        //currentWorker.description.classList.add('selected-worker-description');
+        currentWorker.photo.classList.add('selected-worker');
+    });
+    
+})
+
+
